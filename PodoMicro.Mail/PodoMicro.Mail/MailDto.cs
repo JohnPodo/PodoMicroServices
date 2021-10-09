592b04ae17b2abde91e3ea6f37406d471cbb9ac3
Podo <johnpodogorianiotis@gmail.com>
2021-10-09 13:27:24 +0300

    Add project files.

---
+++ PodoMicro.Mail/PodoMicro.Mail/MailDto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PodoMicro.Mail
+{
+    public class MailDto
+    {
+        /// <summary>
+        /// Your E-mail
+        /// </summary>
+        public string SenderMail { get; set; }
+        /// <summary>
+        /// List of emails to receive email
+        /// </summary>
+        public List<string> Receivers { get; set; }
+        /// <summary>
+        /// Choose the provider of e-mail. Default is gmail.com. Provider must be like "providerName.com" or "providerName.gr"
+        /// </summary>
+        public string Provider { get; set; }
+        /// <summary>
+        /// Encoding of E-mail. Default Encoding is UTF-8
+        /// </summary>
+        public Encoding Encoding { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        /// <summary>
+        /// From which port the email will be sent. Default is 587
+        /// </summary>
+        public int Port { get; set; }
+        /// <summary>
+        /// Subject of email
+        /// </summary>
+        public string Subject { get; set; }
+    }
+}
